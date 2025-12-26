6af8be44874d4cf2bdd5532cf8efe7d805221d96
kirikemae <128600909+kirikemae@users.noreply.github.com>
2025-12-26 19:33:45 +0500

    fix

--- user/internal/domain/entity/user.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-type User struct {
-	ID       int64  `gorm:"primaryKey" json:"id"`
-	Name     string `gorm:"size:255;not null" json:"name"`
-	Email    string `gorm:"size:255;uniqueIndex;not null" json:"email"`
-	Password string `gorm:"size:255;not null" json:"-"`
-	Role     string `gorm:"size:50;not null" json:"role"`
-}
